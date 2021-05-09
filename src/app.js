b92ddf07336ced170f82bbe7c7f2dc9962713d92
Manuel Duarte <carlosd99m@gmail.com>
2021-05-08 23:24:44 -0600

    configurando el servidor local

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
+const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-app.listen(9999, () => {
-  console.log("server on port 9999");
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// setings
+app.set("port", process.env.PORT || 9999);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+
+// routes
+app.use('/', indexRoutes)
+
+// starting the server
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
