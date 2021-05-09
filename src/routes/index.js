d089b120b8f49786ebfab872b3955bb90424b240
Manuel Duarte <carlosd99m@gmail.com>
2021-05-09 01:05:05 -0600

    probando el envio de datos por post

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,10 @@ router.get("/", (req, res) => {
   res.render('index')
 });
 
+router.post("/add", (req, res) => {
+    console.log(req.body)
+    res.send('received');
+
+  });
+
 module.exports = router;
